773beae5011d777cb5a1bb8f6062c7839f9d27e5
Rafael Naville <rafael.nc2@gmail.com>
2025-01-22 08:03:38 -0300

    Criação da camada de aplicação e handler de inclusao de filme

---
+++ CatalogoFilmesSeries.Application/UseCases/Filmes/AdicionarCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using MediatR;
+
+namespace CatalogoFilmesSeries.Application.UseCases.Filmes;
+
+public sealed record AdicionarCommand(
+    string Titulo,
+    string TituloOriginal,
+    int AnoLancamento,
+    int ClassificacaoEtaria,
+    int Duracao,
+    string Sinopse,
+    string UrlImagem
+) : IRequest<ApiResult<AdicionarResponse>>
+{
+    public IEnumerable<string> Categorias { get; private set; } = Enumerable.Empty<string>();
+}
